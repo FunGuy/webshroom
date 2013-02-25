abe23588ad8beeed09cd5aa32da6215ac40f5317
Hannes Landstedt <github.com@zarac.se>
2013-02-25 17:51:38 +0100

    Portal. Server. Client. kinda maybe broken.. hea hea

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
+/**
+ * Fun Guy
+ *  https://github.com/FunGuy
+ */
+
+
 //* Environment
-var WWW_HOST = process.env.WWW_HOST || '0.0.0.0';
-var WWW_PORT = process.env.WWW_PORT || 27015;
+var WWW_HOST = process.env.FUNGUY_WWW_HOST || '0.0.0.0';
+var WWW_PORT = process.env.FUNGUY_WWW_PORT || 8000;
+var CLIENT_SOCKET_HOST = process.env.FUNGUY_CLIENT_SOCKET_HOST || '0.0.0.0';
+var CLIENT_SOCKET_PORT = process.env.FUNGUY_CLIENT_SOCKET_PORT || 8006;
+var SERVER_SOCKET_HOST = process.env.FUNGUY_SOCKET_SERVERS_HOST || '0.0.0.0';
+var SERVER_SOCKET_PORT = process.env.FUNGUY_SOCKET_SERVERS_PORT || 27015;
 
-var SOCKET_PORT = process.env.SOCKET_PORT || 27016;
 
-//* Load services
-var portal_web = require('./portal-web'),
-    portal_socket = require('./portal-socket');
+//* Dependencies
+var net = require('net'),
+    http = require('http'),
+    socketio = require('socket.io'),
+    WebServer = require('./lib/WebServer'),
+    ClientServer = require('./lib/ClientServer'),
+    ServerServer = require('./lib/ServerServer');
 
-//* Init. services
-portal_web.init(WWW_HOST, WWW_PORT);
-portal_socket.init(SOCKET_PORT);
 
-//* Run services
-//* - portal-web
-portal_web.run();
+//* Go!
+var httpServer = http.createServer();
+var clientSocket = socketio.listen(CLIENT_SOCKET_PORT);
+var serverSocket = net.createServer();
 
-//* - portal-socket
-portal_socket.run();
+clientSocket.set('log level', 1);
+
+var portal = new (function(httpServer, clientSocket, serverSocket) {
+    this.foo = 'bar';
+    this.httpServer = httpServer;
+    this.clientSocket = clientSocket;
+    this.serverSocket = serverSocket;
+    this.clients = [];
+    this.servers = [];
+    this.webServer = new WebServer(this, httpServer);
+    this.clientServer = new ClientServer(this, clientSocket);
+    this.serverServer = new ServerServer(this, serverSocket);
+})(httpServer, clientSocket, serverSocket);
+
+httpServer.listen(WWW_PORT, WWW_HOST, function() {
+  console.log("HTTP server listening on %s:%s.", WWW_HOST, WWW_PORT);
+});
+
+serverSocket.listen(SERVER_SOCKET_PORT, SERVER_SOCKET_HOST, function() {
+  console.log("NET server listening on %s:%s.", SERVER_SOCKET_HOST,
+      SERVER_SOCKET_PORT);
+});
